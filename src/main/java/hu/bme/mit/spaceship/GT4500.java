a2704b7ae020d7a5bd33fd7f2badc850d61d49a6
yves1706 <61472507+yves1706@users.noreply.github.com>
2022-04-05 11:23:16 +0200

    newimplementation

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -80,7 +80,7 @@ public class GT4500 implements SpaceShip {
         // try to fire both of the torpedo stores
         //TODO implement feature
 
-        return true;
+        firingSuccess = true;
 
         break;
     }
