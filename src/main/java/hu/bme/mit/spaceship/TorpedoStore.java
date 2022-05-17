77967da85d62490127b62c2df64a191ac7950935
yves1706 <61472507+yves1706@users.noreply.github.com>
2022-05-17 10:39:05 +0200

    Update TorpedoStore.java
    dependency injection

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -11,7 +11,6 @@ public class TorpedoStore {
 
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
-  private Random generator = new Random();
 
   private int torpedoCount = 0;
 
@@ -37,12 +36,12 @@ public class TorpedoStore {
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
-   
+    Random generator = new Random();
     double r = generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
       // successful firing
-      this.torpedoCount -= numberOfTorpedos;
+      this.torpedoCount =  numberOfTorpedos;
       success = true;
     } else {
       // simulated failure
